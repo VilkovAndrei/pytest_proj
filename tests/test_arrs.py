b24678b5e2f2918250704dbc4dadc900969d6a9b
Andry Vilkov <vilkov-l-nn@mail.ru>
2023-12-10 10:57:46 +0300

    Edit tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,10 +2,10 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2  # поправил значение 3 на 2
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
+def test_my_slice():  # поправил название тестовой функции
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
