a05d000ad5e92cba531a38329192a6fecdcbcf6f
Andry Vilkov <vilkov-l-nn@mail.ru>
2023-12-10 15:58:18 +0300

    Add fixture in test_dict.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,19 @@
 from utils import arrs
+import pytest
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2  # поправил значение 3 на 2
+@pytest.fixture
+def fixt_arr():  # имя фикстуры любое
+    return [1, 2, 3, 4]
+
+
+def test_get(fixt_arr):
+    assert arrs.get(fixt_arr, 1, "test") == 2  # поправил значение 3 на 2
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_my_slice():  # поправил название тестовой функции
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_my_slice(fixt_arr):  # поправил название тестовой функции
+    assert arrs.my_slice(fixt_arr, 1, 3) == [2, 3]
+    assert arrs.my_slice(fixt_arr, 1) == [2, 3, 4]
     assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3], None) == [1, 2, 3]
+    assert arrs.my_slice(fixt_arr, None) == [1, 2, 3, 4]
